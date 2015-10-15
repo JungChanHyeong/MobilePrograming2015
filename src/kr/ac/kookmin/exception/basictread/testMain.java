c19f67fb53968fed6ea93e959e350b737f2da0b9
user <user@MSDN-SPECIAL>
2015-10-15 14:17:46 +0900

    Lab07

---
+++ src/kr/ac/kookmin/exception/basictread/testMain.java
@@ -0,0 +1,40 @@
+package kr.ac.kookmin.exception.basictread;
+
+public class testMain {
+
+	public static BankAccount account = new BankAccount();
+
+	public static void main(String[] args) {
+		Thread depositMan = new Thread() {
+
+			public void run() {
+				while (true) {
+					try {
+						sleep(500);
+						account.add(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+
+		Thread withdrawMan = new Thread() {
+
+			public void run() {
+				while (true) {
+					try {
+						sleep(500);
+						account.delete(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+		};
+
+		depositMan.start();
+		withdrawMan.start();
+	}
+}
